dedf5c8c232524e1e95687d1791483b01bf15a7f
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-10-04 10:58:40 +0200

    repr window flicker 1.5.2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,34 +1,49 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(
-            onClick = { text = "Hello, Desktop!" },
-            modifier = Modifier.testTag("button")
-        ) {
-            Text(text)
-        }
-    }
-}
+import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.window.singleWindowApplication
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    val state = rememberWindowState(size = DpSize.Unspecified)
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = state,
+        title = "Adaptive Sizing Flicker Bug",
+        resizable = false,
+    ) {
+        var width by remember { mutableStateOf(480.dp) }
+
+        LaunchedEffect(width) {
+            state.size = DpSize.Unspecified
+        }
+
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .requiredWidth(width)
+                .requiredHeight(width / 2)
+        )
+
+        MenuBar {
+            Menu("View") {
+                Item(text = "480x240", shortcut = KeyShortcut(Key.F1)) { width = 480.dp }
+                Item(text = "800x400", shortcut = KeyShortcut(Key.F2)) { width = 800.dp }
+                Item(text = "1024x512", shortcut = KeyShortcut(Key.F3)) { width = 1024.dp }
+            }
+        }
     }
 }
