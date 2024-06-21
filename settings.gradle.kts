1bfe49198f4ea92c5f7b522d2f6aacb4356c907f
Victor Kropp <victor.kropp@jetbrains.com>
2024-06-21 16:47:23 +0200

    Update template to Compose Multiplatform 1.6.11 and Kotlin 2.0.0 (#12)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,12 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenCentral()
     }
 
     plugins {
         kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
